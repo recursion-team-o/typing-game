c2c2071f3054ce309d47371846dccc74056a1a0a
Yusuke Miura <yusukemiura34c@gmail.com>
2022-03-08 14:39:08 +0900

    aboutViewページからも全てのテンプレートを削除、そしてヘッダーの読み込み

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import HeaderAll from "@/components/HeaderAll.vue";
+</script>
+
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <HeaderAll />
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
