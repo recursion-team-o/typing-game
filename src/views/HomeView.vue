a44370c36365df20d7bd574c5ce533919655dadd
Yusuke Miura <yusukemiura34c@gmail.com>
2022-03-08 14:36:04 +0900

    ヘhomeのページにヘッダーの読み込み、そしてそれの使用

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
+import HeaderAll from "@/components/HeaderAll.vue";
 </script>
 
 <template>
- 
+ <HeaderAll />
 </template>
