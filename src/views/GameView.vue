4584a7ca54beadcd738d364923785b2120e29c56
Yusuke Miura <yusukemiura34c@gmail.com>
2022-03-11 22:59:57 +0900

    #4 GameViewの作成。　GamePage Componentの作成。　その中にHTMLの作成　index.tsにルートリンクを作成

---
+++ src/views/GameView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Header from "@/components/Header.vue";
+import GamePage from "@/components/GamePage.vue";
+
+
+</script>
+
+
+<template class="box">
+  <Header />
+  <GamePage />
+</template>
