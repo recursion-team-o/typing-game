88a9d7f2c7a89b82a49537889e79c5738974016b
Yusuke Miura <yusukemiura34c@gmail.com>
2022-03-11 12:50:18 +0900

    #5 htmlのクラス名をケバぶに、セレクタの後を空白に、listdiscを加えた、ルールをリストから外した

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,7 @@ import HeaderAll from "@/components/HeaderAll.vue";
     <!--welcome userと言語、レベルをいれるための箱。（ここは横並びのためflexboxの利用)-->
     <!--サイズの調整のため透明な横幅60%の箱にいったん入れる-->
     <div class="flex pt-10 justify-center items-center">
-      <div class="welcomebox flex px-10 py-10 items-center justify-around ">
+      <div class="welcome-box flex px-10 py-10 items-center justify-around ">
         <div class="font-bold text-4xl">Welcome User!</div>
         <!--言語とレベルが縦並びなのでさらに箱を一つ加える-->
         <div>
@@ -22,9 +22,8 @@ import HeaderAll from "@/components/HeaderAll.vue";
 
     <!--ルール説明のための箱-->
     <div class="justify-center mt-6 items-center flex">
-      <ol class="rulebox rounded-lg  p-10  bg-gray-100">
-        <li class="text-5xl pb-4">ルール:</li>
-        <li class="text-2xl"> 任意のキーを押すと始まります</li>
+      <ol class="rule-box rounded-lg  p-10  bg-gray-100 list-disc"><span class="text-5xl">ルール</span>
+        <li class="text-2xl pt-5"> 任意のキーを押すと始まります</li>
         <li class="text-2xl"> 左側にソースコードが、右側に自分の書いているコードがでます</li>
         <li class="text-2xl"> 全てのコードを書き終えたら自動的に終了します</li>
         <li class="text-2xl"> レベルはコードを書く量により変わります</li>
@@ -33,7 +32,7 @@ import HeaderAll from "@/components/HeaderAll.vue";
 
     <!--「メインに戻る」と「スタート」のボタンを格納-->
     <div class="pt-20 pb-20 flex items-center justify-center">
-      <div class="flex buttonbox justify-around">
+      <div class="flex button-box justify-around">
         <RouterLink class="inline-block px-20 py-3 rounded-lg shadow-lg bg-indigo-700 text-white" to="/">ホーム画面</RouterLink>
         <a href="#" class="inline-block px-20 py-3 rounded-lg shadow-lg bg-gray-600 text-white">スタート</a>
       </div>
@@ -43,20 +42,20 @@ import HeaderAll from "@/components/HeaderAll.vue";
 </template>
 
 <style>
-.box{
+.box {
   height: 92.2vh;
   width: 100%;
 }
 
-.welcomebox{
+.welcome-box {
   width: 60%;
 }
 
-.rulebox{
+.rule-box {
   width: 70%;
 }
 
-.buttonbox{
+.button-box {
   width:800px;
 }
 </style>
