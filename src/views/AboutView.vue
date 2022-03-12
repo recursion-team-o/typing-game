5b801354aa0695e329c5efcaa89fe1b72350c6f4
Yusuke Miura <yusukemiura34c@gmail.com>
2022-03-12 22:58:09 +0900

    スペース削除

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,7 @@ import HeaderAll from "@/components/HeaderAll.vue";
     <!--welcome userと言語、レベルをいれるための箱。（ここは横並びのためflexboxの利用)-->
     <!--サイズの調整のため透明な横幅60%の箱にいったん入れる-->
     <div class="flex pt-10 justify-center items-center">
-      <div class="welcome-box flex px-10 py-10 items-center justify-around ">
+      <div class="welcome-box flex px-10 py-10 items-center justify-around">
         <div class="font-bold text-4xl">Welcome User!</div>
         <!--言語とレベルが縦並びなのでさらに箱を一つ加える-->
         <div>
@@ -22,22 +22,25 @@ import HeaderAll from "@/components/HeaderAll.vue";
 
     <!--ルール説明のための箱-->
     <div class="justify-center mt-6 items-center flex">
-      <ol class="rule-box rounded-lg  p-10  bg-gray-100 list-disc"><span class="text-5xl">ルール</span>
-        <li class="text-2xl pt-5"> 任意のキーを押すと始まります</li>
-        <li class="text-2xl"> 左側にソースコードが、右側に自分の書いているコードがでます</li>
-        <li class="text-2xl"> 全てのコードを書き終えたら自動的に終了します</li>
-        <li class="text-2xl"> レベルはコードを書く量により変わります</li>
+      <ol class="rule-box rounded-lg p-10 bg-gray-100 list-disc">
+        <span class="text-5xl">ルール</span>
+        <li class="text-2xl pt-5">任意のキーを押すと始まります</li>
+        <li class="text-2xl">左側にソースコードが、右側に自分の書いているコードがでます</li>
+        <li class="text-2xl">全てのコードを書き終えたら自動的に終了します</li>
+        <li class="text-2xl">レベルはコードを書く量により変わります</li>
       </ol>
     </div>
 
     <!--「メインに戻る」と「スタート」のボタンを格納-->
     <div class="pt-20 pb-20 flex items-center justify-center">
       <div class="flex button-box justify-around">
-        <RouterLink class="inline-block px-20 py-3 rounded-lg shadow-lg bg-indigo-700 text-white" to="/">ホーム画面</RouterLink>
+        <RouterLink
+          class="inline-block px-20 py-3 rounded-lg shadow-lg bg-indigo-700 text-white"
+          to="/"
+        >ホーム画面</RouterLink>
         <a href="#" class="inline-block px-20 py-3 rounded-lg shadow-lg bg-gray-600 text-white">スタート</a>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -56,6 +59,6 @@ import HeaderAll from "@/components/HeaderAll.vue";
 }
 
 .button-box {
-  width:800px;
+  width: 800px;
 }
 </style>
