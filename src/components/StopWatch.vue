0b65ce947d3957635ad0a5453d7dff7edeb36d8e
hata <wmf8341@gmail.com>
2022-03-25 14:04:26 +0900

    #57 タイマーの高さ修正

--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -8,7 +8,7 @@ const { startTimer, stopTimer, resetTimer } = timer;
 </script>
 
 <template>
-  <div class="flex items-center justify-center border bg-gray-200 w-3/12 h-24">
+  <div class="flex items-center justify-center border bg-gray-200 w-3/12 h-12">
     <div class="flex justify-center items-center h-10 mx-auto">
       <p class="text-black text-2xl w-[33px]">
         {{ getMinString }}
@@ -21,26 +21,6 @@ const { startTimer, stopTimer, resetTimer } = timer;
       <p class="text-black text-2xl w-[33px]">
         {{ getPointMsec }}
       </p>
-    </div>
-    <div class="flex flex-col justify-center items-center">
-      <button
-        class="border w-24 mb-2 bg-red-200 rounded-lg uppercase shadow hover:border-red-400"
-        @click="startTimer()"
-      >
-        start
-      </button>
-      <button
-        class="border w-24 mb-2 bg-blue-200 rounded-lg uppercase shadow hover:border-blue-400"
-        @click="stopTimer()"
-      >
-        stop
-      </button>
-      <button
-        class="border w-24 bg-purple-400 rounded-lg uppercase shadow hover:border-blue-400"
-        @click="resetTimer()"
-      >
-        reset
-      </button>
     </div>
   </div>
 </template>
