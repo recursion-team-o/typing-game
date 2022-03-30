00ee5f719be3c5078f860fc295f8c3c317339a2d
hata <wmf8341@gmail.com>
2022-03-30 18:50:31 +0900

    #64 コードの修正

--- src/components/HomeLeftContainer.vue
+++ src/components/HomeLeftContainer.vue
@@ -3,30 +3,44 @@ import LevelDialog from "../components/LevelDialog.vue";
 import { ref, reactive, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { userStore } from "../stores/user";
+
 let showLevelDialog = ref<boolean>(false);
 const langMenu: string[] = ["JavaScript", "Java"];
 const levelMenu: string[] = ["初級", "中級", "上級"];
 const user = userStore();
-const { lang, checkLang, level, checkLevel } = storeToRefs(user);
-let langStatus = ref<boolean>(false);
-let levelStatus = ref<boolean>(false);
+const { lang, level } = storeToRefs(user);
+
+const errorLangObj: {
+  text: string;
+  show: boolean;
+} = reactive({
+  text: "言語を選択してください",
+  show: false,
+});
+
+const errorLevelObj: {
+  text: string;
+  show: boolean;
+} = reactive({
+  text: "レベルを選択してください",
+  show: false,
+});
+
 const langInput = computed({
-  get: () => langStatus.value,
-  set: (value: boolean) => (langStatus.value = value),
+  get: () => errorLangObj.show,
+  set: (value: boolean) => (errorLangObj.show = value),
 });
+
 const levelInput = computed({
-  get: () => levelStatus.value,
-  set: (value: boolean) => (levelStatus.value = value),
+  get: () => errorLevelObj.show,
+  set: (value: boolean) => (errorLevelObj.show = value),
 });
-const checkInput = (check: any): boolean => {
-  if (!check.value) return true;
-  else return false;
-};
+
 const openDialog = (): void => {
-  if (checkInput(checkLang) || checkInput(checkLevel)) {
-    langInput.value = checkInput(checkLang);
-    levelInput.value = checkInput(checkLevel);
-    alert("必須項目を入力してください");
+  if (lang.value == "" || level.value == "") {
+    langInput.value = lang.value == "";
+    levelInput.value = level.value == "";
+
     return;
   }
   showLevelDialog.value = true;
@@ -34,7 +48,8 @@ const openDialog = (): void => {
 const closeDialog = (): void => {
   showLevelDialog.value = false;
 };
-let errorClass = reactive({
+
+let errorLangClass = reactive({
   error: langInput,
 });
 let errorLevelClass = reactive({
@@ -45,13 +60,39 @@ let errorLevelClass = reactive({
 <template>
   <div class="flex justify-center items-center w-1/2 h-full">
     <div
-      class="flex flex-col justify-center items-center max-w-md w-10/12 bg-gray-100 border border-gray-900 border-opacity-60 rounded-lg shadow-lg"
+      class="flex flex-col justify-center items-center max-w-md w-10/12 bg-gray-100 border border-gray-900 border-opacity-60 rounded-lg shadow-lg relative"
     >
+      <div
+        v-show="langInput || levelInput"
+        class="flex justify-center items-center mb-4 text-sm text-red-700 bg-red-100 rounded-lg text-center absolute z-10 w-10/12 h-1/4 top-2 transition-all duration-200"
+        role="alert"
+      >
+        <svg
+          class="inline flex-shrink-0 mr-3 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <div>
+          <span v-show="langInput" class="font-medium block">{{
+            errorLangObj.text
+          }}</span>
+          <span v-show="levelInput" class="font-medium block">{{
+            errorLevelObj.text
+          }}</span>
+        </div>
+      </div>
       <h3 class="py-6 font-semibold text-2xl">レベル別練習</h3>
       <div class="relative inline-block w-7/12 mb-4 text-gray-700">
         <select
           @change="langInput = false"
-          :class="errorClass"
+          :class="errorLangClass"
           class="w-full h-10 pl-3 pr-6 text-base border rounded-lg appearance-none focus:shadow-outline"
           v-model="lang"
         >
--- src/components/HomeRightContainer.vue
+++ src/components/HomeRightContainer.vue
@@ -6,20 +6,17 @@ import { storeToRefs } from "pinia";
 
 let showMyCodeDialog = ref<boolean>(false);
 const code = codeStore();
-const { sampleCode, checkSampleCode } = storeToRefs(code);
+const { sampleCode } = storeToRefs(code);
 let codeStatus = ref<boolean>(false);
+
 const sampleCodeInput = computed({
   get: () => codeStatus.value,
   set: (value: boolean) => (codeStatus.value = value),
 });
-const checkInput = (check: any): boolean => {
-  if (!check.value) return true;
-  else return false;
-};
+
 const openDialog = (): void => {
-  if (checkInput(checkSampleCode)) {
-    sampleCodeInput.value = checkInput(checkSampleCode);
-    alert("必須項目を入力してください");
+  if (sampleCode.value == "") {
+    sampleCodeInput.value = sampleCode.value == "";
     return;
   }
   showMyCodeDialog.value = true;
@@ -27,6 +24,7 @@ const openDialog = (): void => {
 const closeDialog = (): void => {
   showMyCodeDialog.value = false;
 };
+
 let errorClass = reactive({
   error: sampleCodeInput,
 });
@@ -35,8 +33,31 @@ let errorClass = reactive({
 <template>
   <div class="flex justify-center items-center w-1/2 h-full">
     <div
-      class="flex flex-col justify-center items-center max-w-md w-10/12 bg-gray-100 border border-gray-900 border-opacity-60 rounded-lg shadow-lg"
+      class="flex flex-col justify-center items-center max-w-md w-10/12 bg-gray-100 border border-gray-900 border-opacity-60 rounded-lg shadow-lg relative"
     >
+      <div
+        v-show="sampleCodeInput"
+        class="flex justify-center items-center mb-4 text-sm text-red-700 bg-red-100 rounded-lg text-center absolute z-10 w-10/12 h-1/4 top-2 transition-all duration-200"
+        role="alert"
+      >
+        <svg
+          class="inline flex-shrink-0 mr-3 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <div>
+          <span v-show="sampleCodeInput" class="font-medium block"
+            >コードをペーストしてください</span
+          >
+        </div>
+      </div>
       <h3 class="py-6 font-semibold text-2xl">自分のソースコード練習</h3>
       <div class="w-7/12 mb-4">
         <textarea
