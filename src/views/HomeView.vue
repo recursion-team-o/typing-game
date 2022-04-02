826a7746ac94ded4fe03c155184c23a50b13b1f2
hata8341 <83937097+hata8341@users.noreply.github.com>
2022-04-02 10:42:27 +0900

    google ゲスト 認証機能の作成 (#71)
    * #52 ヘッダーの集約と各pageの高さ修正
    
    * #57 ヘッダータイマーの表示の修正
    
    * #57 esLintの修正
    
    * #57 タイマーの高さ修正
    
    * #57 タイマー表示処理の簡略化
    
    * #59 bgmとseの再生機能の追加
    
    * #59 lintFix
    
    * #59 音量調節修正
    
    * #62 ヘッダーBGMの追加とクリック音の追加
    
    * #64 入力項目の修正
    
    * #64 flowbiteのインストール
    
    * #64 checkメソッドの削除
    
    * #64 コードの修正
    
    * #64 relativeの削除
    
    * #68 firebaseの初期設定
    
    * #68 認証機能の作成
    
    * #68 エラーメッセージの表示

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,15 @@ import HomeLeftContainer from "../components/HomeLeftContainer.vue";
 import HomeRightContainer from "../components/HomeRightContainer.vue";
 import { userStore } from "../stores/user";
 import { storeToRefs } from "pinia";
+import { authStore } from "../stores/auth";
 
 const user = userStore();
 const { name } = storeToRefs(user);
+
+const auth = authStore();
+const { currentUser, isLoggedIn } = storeToRefs(auth);
+
+auth.getAuthState();
 </script>
 
 <template>
@@ -14,21 +20,108 @@ const { name } = storeToRefs(user);
       <div class="flex flex-col justify-center text-center h-1/2">
         <p>A website focused on improving your coding skills</p>
         <h2 class="text-5xl my-4">CODE-TYPING</h2>
-        <div class="flex mx-auto h-4/12">
-          <div class="flex relative">
-            <span
-              class="rounded-l-md inline-flex items-center px-3 border-t bg-white border-l border-b border-gray-300 text-gray-500 shadow-sm text-sm"
+        <div class="">
+          <button
+            v-show="!isLoggedIn"
+            @click="auth.login()"
+            type="button"
+            class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
+          >
+            <svg
+              class="w-4 h-4 mr-2 -ml-1"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="google"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
             >
-              UserName
-            </span>
-            <input
-              v-model="name"
-              type="text"
-              class="rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base"
-              name="userName"
-              placeholder="ユーザーネームを入力"
-            />
+              <path
+                fill="currentColor"
+                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+              ></path>
+            </svg>
+            Sign in with Google
+          </button>
+
+          <div
+            v-show="isLoggedIn"
+            class="flex justify-center items-center space-x-4"
+          >
+            <div class="flex items-center space-y-1 font-medium">
+              <div>
+                {{
+                  currentUser?.displayName != null
+                    ? currentUser?.displayName
+                    : "ゲスト"
+                }}さん
+              </div>
+              <img
+                v-show="currentUser?.displayName != null"
+                class="w-10 h-10 rounded-full"
+                :src="currentUser?.photoURL"
+                alt=""
+              />
+
+              <div
+                v-show="currentUser?.displayName == null"
+                class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"
+              >
+                <svg
+                  class="absolute w-12 h-12 text-gray-400 -left-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
           </div>
+
+          <button
+            v-show="isLoggedIn"
+            @click="auth.logout()"
+            class="w-2/12 border border-b-red-400 bg-red-100"
+          >
+            ログアウト
+          </button>
+        </div>
+        <div>
+          <a
+            v-show="!isLoggedIn"
+            @click="auth.guestsLogin()"
+            class="relative inline-flex items-center justify-center p-4 px-6 py-3 bg-white overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group cursor-pointer"
+          >
+            <span
+              class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span
+              class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease"
+              >ゲスト ログイン</span
+            >
+            <span class="relative invisible">Button Text</span>
+          </a>
         </div>
       </div>
       <div class="flex h-1/2">
